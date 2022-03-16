1c8370dd744187c7b1500eb20baa8ffea5f1dbd1
Ciara Gilsenan <ciaragil98@gmail.com>
2022-03-16 13:00:38 +0000

    change train file name

--- code/algorithms.py
+++ code/algorithms.py
@@ -33,7 +33,7 @@ def plot_roc():
 if __name__ == "__main__":
     
     # read in training set
-    df_train = pd.read_json('../train_output.json')
+    df_train = pd.read_json('../data/pre_train.json')
     
     x1 = np.array(df_train['posts'])
     x2 = np.array(df_train['genders'])
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     x_train = np.column_stack((x1, x2))
 
     # read in data json
-    df_test = pd.read_json('../test_output.json')
+    df_test = pd.read_json('../data/pre_test.json')
 
     x1 = np.array(df_test['posts'])
     x2 = np.array(df_test['genders'])
@@ -50,13 +50,11 @@ if __name__ == "__main__":
 
     x_test = np.column_stack((x1, x2))
     
-    # split data into train and test sets
-
     # algorithm 1 predictions
     predictionsLogistic, logisticModel = LogisticRegressionFunction(x_train, y_train, x_test, y_test)
     
     # algorithm 2 predictions
-    #predictionsKnn, knnModel = KnnFunction(x_train, x_test, y_train, y_test)
+    predictionsKnn, knnModel = KnnFunction(x_train, x_test, y_train, y_test)
 
     # algorithm 3 predictions
     predictionsDeepLearning, deepLModel = deep_learning_algorithm(x_train, x_test, y_train, y_test)
