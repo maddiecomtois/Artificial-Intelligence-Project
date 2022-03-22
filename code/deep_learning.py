8e935e90f5080fb203c47dd203c4b0d8a66047c3
Maddie <13711456+maddiecomtois@users.noreply.github.com>
2022-03-22 10:33:55 +0000

    delete unnecessary code

--- code/deep_learning.py
+++ code/deep_learning.py
@@ -2,34 +2,6 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.feature_extraction.text import TfidfTransformer
 
 def deep_learning_algorithm(X_train, X_test, y_train, y_test):
-    # read in the data
-    '''
-    df = data
-    print(df.shape)
-    df.describe().transpose()
-
-    # column we want to predict
-    target_column = ['age']
-    # set of features (minus target)
-    predictors = list(set(list(df.columns))-set(target_column))
-    print(predictors)
-
-    # normalise the features
-    #df[predictors] = df[predictors]/df[predictors].max()
-    # values are scaled between 0 and 1
-    df.describe().transpose()
-
-    # create the x and y variables
-    X = df[predictors].values
-    y = df[target_column].values
-
-    print(X)
-
-    # split into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=40)
-    print(X_train.shape); print(X_test.shape)
-    '''
-
     # use tf-idf to give weights to the words
     tfidf_transformer = TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train)
